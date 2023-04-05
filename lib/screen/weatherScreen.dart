e83182fe736f0e48f981803a4635944d11b41a0c
Ajrdn <94908806+Ajrdn@users.noreply.github.com>
2023-04-06 08:07:59 +0900

    weatherScreen

---
+++ lib/screen/weatherScreen.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/components/currentWeatherComponent.dart';
+import 'package:weather_app/components/hourlyWeatherComponent.dart';
+import 'package:weather_app/components/dailyWeatherComponent.dart';
+import 'package:weather_app/components/weatherAppBarComponent.dart';
+import 'package:weather_app/models/currentWeatherTitle.dart';
+import 'package:weather_app/models/dailyWeather.dart';
+import 'package:weather_app/models/hourlyWeather.dart';
+import 'package:weather_app/utils/data_utils.dart';
+import 'package:weather_icons/weather_icons.dart';
+import 'package:gap/gap.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  String? city;
+  int? temperature;
+  String? state;
+  DateTime? now;
+  List<CurrentWeatherTitle>? weatherTitleList;
+  List<HourlyWeather>? hourlyWeatherList;
+  List<DailyWeather>? dailyWeatherList;
+
+  Future<void> refreshWeatherData() async {
+    final data = await getWeatherData();
+
+    setState(() {
+      city = data!.currentWeather!.city;
+      temperature = data!.currentWeather!.temperature;
+      state = data!.currentWeather!.state;
+      now = data!.currentWeather!.now;
+      List<CurrentWeatherTitle> weatherTitleList = [
+        CurrentWeatherTitle(
+          title: 'Sunup',
+          data: '${(data!.currentWeather!.sunup.hour != 12 ? data!.currentWeather!.sunup.hour % 12 : data!.currentWeather!.sunup.hour).toString().padLeft(2, '0')}:${data!.currentWeather!.sunup.minute.toString().padLeft(2, '0')}',
+          unit: 'a.m.',
+          icon: const BoxedIcon(
+            WeatherIcons.sunrise,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Sundown',
+          data: '${(data!.currentWeather!.sundown.hour != 12 ? data!.currentWeather!.sundown.hour % 12 : data!.currentWeather!.sundown.hour).toString().padLeft(2, '0')}:${data!.currentWeather!.sundown.minute.toString().padLeft(2, '0')}',
+          unit: 'p.m.',
+          icon: const BoxedIcon(
+            WeatherIcons.sunset,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Wind Chill',
+          data: '${data!.currentWeather!.windChill}',
+          unit: '°C',
+          icon: const BoxedIcon(
+            WeatherIcons.thermometer_exterior,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Visibility',
+          data: data!.currentWeather!.visibility < 1000 ? '${data!.currentWeather!.visibility}m' : '${data!.currentWeather!.visibility % 1000 == 0 ? (data!.currentWeather!.visibility / 1000).round() : data!.currentWeather!.visibility / 1000}',
+          unit: 'km',
+          icon: const BoxedIcon(
+            WeatherIcons.day_fog,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Clouds',
+          data: '${data!.currentWeather!.clouds}',
+          unit: '%',
+          icon: const BoxedIcon(
+            WeatherIcons.cloud,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Humidity',
+          data: '${data!.currentWeather!.humidity}',
+          unit: '%',
+          icon: const BoxedIcon(
+            WeatherIcons.raindrop,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Pressure',
+          data: '${data!.currentWeather!.pressure}',
+          unit: 'hPa',
+          icon: const BoxedIcon(
+            WeatherIcons.barometer,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+        CurrentWeatherTitle(
+          title: 'Wind Speed',
+          data: '${data!.currentWeather!.windSpeed}',
+          unit: 'm/s',
+          icon: const BoxedIcon(
+            WeatherIcons.strong_wind,
+            color: Colors.white70,
+            size: 20.0,
+          ),
+        ),
+      ];
+      hourlyWeatherList = data!.hourlyWeatherList;
+      dailyWeatherList = data!.dailyWeatherList;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: getWeatherData(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          else if(snapshot.hasError)
+            return Center(
+              child: Text('에러가 발생하였습니다. 다시 시도해 주십시오.'),
+            );
+
+          final data = snapshot.data;
+
+          city = data!.currentWeather!.city;
+          temperature = data!.currentWeather!.temperature;
+          state = data!.currentWeather!.state;
+          now = data!.currentWeather!.now;
+          List<CurrentWeatherTitle> weatherTitleList = [
+            CurrentWeatherTitle(
+              title: 'Sunup',
+              data: '${(data!.currentWeather!.sunup.hour != 12 ? data!.currentWeather!.sunup.hour % 12 : data!.currentWeather!.sunup.hour).toString().padLeft(2, '0')}:${data!.currentWeather!.sunup.minute.toString().padLeft(2, '0')}',
+              unit: 'a.m.',
+              icon: const BoxedIcon(
+                WeatherIcons.sunrise,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Sundown',
+              data: '${(data!.currentWeather!.sundown.hour != 12 ? data!.currentWeather!.sundown.hour % 12 : data!.currentWeather!.sundown.hour).toString().padLeft(2, '0')}:${data!.currentWeather!.sundown.minute.toString().padLeft(2, '0')}',
+              unit: 'p.m.',
+              icon: const BoxedIcon(
+                WeatherIcons.sunset,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Wind Chill',
+              data: '${data!.currentWeather!.windChill}',
+              unit: '°C',
+              icon: const BoxedIcon(
+                WeatherIcons.thermometer_exterior,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Visibility',
+              data: data!.currentWeather!.visibility < 1000 ? '${data!.currentWeather!.visibility}m' : '${data!.currentWeather!.visibility % 1000 == 0 ? (data!.currentWeather!.visibility / 1000).round() : data!.currentWeather!.visibility / 1000}',
+              unit: 'km',
+              icon: const BoxedIcon(
+                WeatherIcons.day_fog,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Clouds',
+              data: '${data!.currentWeather!.clouds}',
+              unit: '%',
+              icon: const BoxedIcon(
+                WeatherIcons.cloud,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Humidity',
+              data: '${data!.currentWeather!.humidity}',
+              unit: '%',
+              icon: const BoxedIcon(
+                WeatherIcons.raindrop,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Pressure',
+              data: '${data!.currentWeather!.pressure}',
+              unit: 'hPa',
+              icon: const BoxedIcon(
+                WeatherIcons.barometer,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+            CurrentWeatherTitle(
+              title: 'Wind Speed',
+              data: '${data!.currentWeather!.windSpeed}',
+              unit: 'm/s',
+              icon: const BoxedIcon(
+                WeatherIcons.strong_wind,
+                color: Colors.white70,
+                size: 20.0,
+              ),
+            ),
+          ];
+          hourlyWeatherList = data!.hourlyWeatherList;
+          dailyWeatherList = data!.dailyWeatherList;
+
+          return Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12.0,
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/sky.jpg'),
+              ),
+            ),
+            child: RefreshIndicator(
+              onRefresh: refreshWeatherData,
+              child: CustomScrollView(
+                slivers: [
+                  WeatherAppBarComponent(
+                    city: city!,
+                    temperature: temperature!,
+                    state: state!,
+                    now: now!,
+                  ),
+                  CurrentWeatherComponent(
+                    weatherTitleList: weatherTitleList!,
+                  ),
+                  const SliverGap(70),
+                  HourlyWeatherComponent(
+                    hourlyWeatherList: hourlyWeatherList!,
+                  ),
+                  const SliverGap(70),
+                  DailyWeatherComponent(
+                    dailyWeatherList: dailyWeatherList!,
+                  ),
+                  const SliverGap(30),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
